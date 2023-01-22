697d43425f195e1eeef382b64f47bdfde30ced29
Abolfazl <a1382keshavarz@gmail.com>
2023-01-22 11:05:41 +0330

    add files

--- Activities/Day1.py
+++ Activities/Day1.py
@@ -1,7 +1,14 @@
 import numpy as np
 import cv2 as cv
 
-Img1 = np.zeros((3, 3), dtype=np.uint8)
+Img1 = np.zeros((5, 3), dtype=np.uint8)
 print(Img1)
+print(Img1.shape)
 Img1 = cv.cvtColor(Img1, cv.COLOR_GRAY2BGR)
 print(Img1)
+print(Img1.shape)
+
+ImgPC = cv.imread(r'C:\Users\Turbo\Desktop\org.jpg', cv.IMREAD_UNCHANGED)
+print(ImgPC)
+print(ImgPC.shape)
+cv.imwrite("RGB.png",ImgPC)
