7a33f8bdb0970881175c3d9edd9f494a51a5ee8d
Abolfazl <abolfazlkeshavarz1112@gmail.com>
2023-02-08 16:24:56 +0330

    add Week3 & rename some files

---
+++ Activities/Week 3/main.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+import numpy as np
+from scipy import ndimage
+
+KernelMatrix_3x3 = np.array([[-1,-1,-1],
+                             [-1, 8,-1],
+                             [-1,-1,-1]])
+KernelMatrix_5x5 = np.array([[-1,-1,-1,-1,-1],
+                             [-1, 1, 2, 1,-1],
+                             [-1, 2, 4, 2,-1],
+                             [-1, 1, 2, 1,-1],
+                             [-1,-1,-1,-1,-1]])
+
+MinionIMG = cv.imread(r"C:\Users\Turbo\Desktop\Minions.jpg", 0)
+
+MinionK3 = ndimage.convolve(MinionIMG, KernelMatrix_3x3)
+MinionK5 = ndimage.convolve(MinionIMG, KernelMatrix_5x5)
+cv.imshow("3x3", MinionK3)
+cv.imshow("5x5", MinionK5)
+cv.waitKey(0)
+
